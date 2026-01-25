51413f4468de1ef49d1cd90457827d3bf2ca0619
Prahlad Panthi <prahladpanthi.work@gmail.com>
2026-01-25 14:59:07 +1100

    initial setup for the lab

---
+++ app/pages/index.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items: NavigationMenuItem[][] = [
+  [
+    {
+      label: "Dashboard",
+      icon: "i-lucide-layout-dashboard",
+    },
+    {
+      label: "About",
+      icon: "i-lucide-info",
+    },
+    {
+      label: "Components",
+      icon: "i-lucide-component",
+      children: [
+        {
+          label: "Buttons",
+          icon: "i-lucide-square-arrow-right",
+        },
+        {
+          label: "Cards",
+          icon: "i-lucide-id-card",
+        },
+      ],
+    },
+  ],
+  [
+    {
+      label: "Settings",
+      icon: "i-lucide-settings",
+    },
+  ],
+];
+</script>
+
+<template>
+  <UDashboardGroup>
+    <UDashboardSidebar collapsible resizable>
+      <UNavigationMenu :items="items[0]" orientation="vertical" />
+      <UNavigationMenu
+        :items="items[1]"
+        orientation="vertical"
+        class="mt-auto"
+      />
+    </UDashboardSidebar>
+    <slot />
+  </UDashboardGroup>
+</template>
